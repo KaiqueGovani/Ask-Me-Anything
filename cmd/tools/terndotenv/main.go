858b5335f2b5713240eea25ac7afeb2c16ec84cc
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2024-08-06 11:35:05 -0300

    Setting up Postgres and first Migrations with tern and sqlc

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Command failed with error: %v\n", err)
+		fmt.Printf("Command output: %s\n", string(output))
+		panic(err)
+	}
+
+	fmt.Printf("Command output: %s\n", string(output))
+}
